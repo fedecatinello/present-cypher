bfef0d1f0b3f49cfec8ee13111805460b74246c5
Federico <fcatinello@gmail.com>
2016-10-23 17:26:19 -0300

    Implementing cypher operations

--- present.c
+++ present.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iso646.h>
 #include "present.h"
 
 /**
@@ -10,32 +11,57 @@
  * **/
 
 
+// UTIL FUNCTIONS
+
+char* convertToString(int32_t num, int32_t size_buffer) {
+
+    char buffer[size_buffer];
+    sprintf( buffer, "%d", num);
+    buffer[size_buffer] = '\0';
+    return buffer;
+}
+
+int32_t convertToInteger(char* buffer) {
+
+    return atoi(buffer);
+}
+
+
+// PRESENT CYPHER FUNCTIONS
+
 input_t* init(char** args){
 
     input_t* data = malloc(sizeof(int32_t)*3+strlen(args[2])+strlen(args[3]));
 
     data->rounds = 32;   //By default algorithm manages 32 rounds
-    data->encrypt = !strcmp(args[1], "encrypt");
+    data->encrypt = (not strcmp(args[1], "encrypt"));
     data->_80bitkey = strlen(args[3]) == 10;
 
-    data->key = malloc(strlen(args[3]));
-    strcpy(data->key, args[3]);
-
     data->text = malloc(strlen(args[2]));
-    strcpy(data->text, args[2]);
+    data->text = (unsigned char*)args[2];
+
+    data->key = malloc(strlen(args[3]));
+    data->key = (unsigned char*)args[3];
 
     return data;
 }
 
 void generateRoundKeys(input_t* data) {
 
+}
+
+void addRoundKey(uint8_t* state[], uint8_t* roundKey[]){
+
+
 
 }
 
 
+// MAIN PROGRAM
+
 int main(int argc, char** argv) {
 
-    if(argc == 2 && !strcmp(argv[1], "help")) {
+    if(argc == 2 and (not strcmp(argv[1], "help"))) {
         printf(ANSI_COLOR_BOLDCYAN "Este programa recibe tres parametros: ");
         printf(ENTER);
         printf("1. El tipo de operacion ---> encrypt o decrypt \n");
@@ -44,7 +70,7 @@ int main(int argc, char** argv) {
         return EXIT_SUCCESS;
     }
 
-    if(argc != 4) {
+    if(argc not_eq 4) {
         printf(ANSI_COLOR_BOLDRED "Cantidad erronea de parametros. Este programa recibe tres parametros" ANSI_COLOR_RESET);
         printf(ENTER);
         printf(ANSI_COLOR_BOLDYELLOW "Ingrese help para recibir ayuda" ANSI_COLOR_RESET);
@@ -53,7 +79,41 @@ int main(int argc, char** argv) {
 
     input_t* data = init(argv);
 
-    generateRoundKeys(data);
+    uint8_t round_counter = 1;
+    uint8_t state[8];
+    uint8_t round_key[10];
+
+    /** Add round key **/
+
+    state[0] = data->text[0] xor data->key[0];
+    state[1] = data->text[1] xor data->key[1];
+    state[2] = data->text[2] xor data->key[2];
+    state[3] = data->text[3] xor data->key[3];
+    state[4] = data->text[4] xor data->key[4];
+    state[5] = data->text[5] xor data->key[5];
+    state[6] = data->text[6] xor data->key[6];
+    state[7] = data->text[7] xor data->key[7];
+
+
+    /** Update round key **/
+
+    round_key[9] = data->key[6] << 5 bitor data->key[7] >> 3;
+    round_key[8] = data->key[5] << 5 bitor data->key[6] >> 3;
+    round_key[7] = data->key[4] << 5 bitor data->key[5] >> 3;
+    round_key[6] = data->key[3] << 5 bitor data->key[4] >> 3;
+    round_key[5] = data->key[2] << 5 bitor data->key[3] >> 3;
+    round_key[4] = data->key[1] << 5 bitor data->key[2] >> 3;
+    round_key[3] = data->key[0] << 5 bitor data->key[1] >> 3;
+    round_key[2] = data->key[9] << 5 bitor data->key[0] >> 3;
+    round_key[1] = data->key[8] << 5 bitor data->key[9] >> 3;
+    round_key[0] = data->key[7] << 5 bitor data->key[8] >> 3;
+
+    round_key[0] = (round_key[0] bitand 0x0F) bitor sbox[round_key[0] >> 4];
+
+    round_key[7] xor_eq round_counter >> 1;
+    round_key[8] xor_eq round_counter << 7;
+
+//    generateRoundKeys(data);
 
 //    for(int i = 1; i<(data->rounds)-1; i++) {
 //          addRoundKey(state,Ki)
--- present.h
+++ present.h
@@ -2,6 +2,8 @@
 #ifndef PRESENT_CYPHER_PRESENT_H
 #define PRESENT_CYPHER_PRESENT_H
 
+#include <stdint.h>
+
 #define ENTER "\n\n\n"
 #define ANSI_COLOR_RED     "\x1b[31m"
 #define ANSI_COLOR_GREEN   "\x1b[32m"
@@ -25,13 +27,27 @@
 typedef struct input_data {
     int32_t rounds;
     int32_t encrypt;  //Flag that indicate if operation is encrypt or decrypt
-    char* key;
+    const uint8_t* key;
     int32_t _80bitkey;  //Flag that indicate if key has 80-bit length or 128-bit length
-    char* text;
+    const uint8_t* text;
 
 }input_t;
 
+
+// Bit Substitution Layer
+static const uint8_t Sbox[16] = {0xc,0x5,0x6,0xb,0x9,0x0,0xa,0xd,0x3,0xe,0xf,0x8,0x4,0x7,0x1,0x2};
+
+// Bit Permutation Layer
+static const uint8_t Pbox[] = {0,16,32,48,1,17,33,49,2,18,34,50,3,19,35,51,
+                              4,20,36,52,5,21,37,53,6,22,38,54,7,23,39,55,
+                              8,24,40,56,9,25,41,57,10,26,42,58,11,27,43,59,
+                              12,28,44,60,13,29,45,61,14,30,46,62,15,31,47,63};
+
 input_t* init(char**);
 void generateRoundKeys(input_t*);
+
+// UTIL FUNCTIONS
+char* convertToString(int32_t, int32_t);
+int32_t convertToInteger(char*);
 
 #endif //PRESENT_CYPHER_PRESENT_H
